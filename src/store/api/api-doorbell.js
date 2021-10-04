9ae2e97fabfd96827517f3a9aa605ec4ba283fd7
gunn <gunnkeem@icloud.com>
2021-10-04 12:41:50 +0900

    NEW | public-ip module => pushCliInfo Action

--- src/store/api/api-doorbell.js
+++ src/store/api/api-doorbell.js
@@ -1,8 +1,10 @@
+const publicIp = require('public-ip');
+
 export default {
   namespaced: false,
   
   state: () => ({
-
+    cli_ip: '0.0.0.0'
 
   }),
 
@@ -12,12 +14,17 @@ export default {
   },
 
   mutations: {
-
+    setCliInfo(state, payload) {
+      state.cli_ip = payload.ipv4
+    }
 
   },
 
   actions: {
+    async pushCliInfo({ commit }) {
+      const ipv4 = await publicIp.v4();
+      commit('setCliInfo', { ipv4 });
+    }
 
-    
   }
 }
